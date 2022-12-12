9093260445495687a5ea9dfb5ce04a6ede78ccf2
VeliAliyev <veli_aliyev@outlook.com>
2022-12-12 22:01:32 +0300

    controller tests added

--- src/main/java/com/velialiyev/microservicesbeerservice/web/controller/BeerController.java
+++ src/main/java/com/velialiyev/microservicesbeerservice/web/controller/BeerController.java
@@ -1,5 +1,6 @@
 package com.velialiyev.microservicesbeerservice.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.velialiyev.microservicesbeerservice.web.model.BeerDto;
 import com.velialiyev.microservicesbeerservice.web.model.BeerPagedList;
 import org.springframework.http.HttpStatus;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@RestController("/api/v1/beer")
+@RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
 //    @GetMapping
@@ -16,9 +18,9 @@ public class BeerController {
 //        return null;
 //    }
 
-    @GetMapping("{/uuid}")
-    public ResponseEntity<BeerDto> getBeerById(UUID uuid){
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.CREATED);
+    @GetMapping("/{uuid}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID uuid){
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
     }
 
     @PostMapping
@@ -26,8 +28,8 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-    @PutMapping("{/uuid}")
-    public ResponseEntity<String> updateBeerById(UUID uuid, @RequestBody BeerDto beerDto){
+    @PutMapping("/{uuid}")
+    public ResponseEntity<String> updateBeerById(@PathVariable UUID uuid, @RequestBody BeerDto beerDto){
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
