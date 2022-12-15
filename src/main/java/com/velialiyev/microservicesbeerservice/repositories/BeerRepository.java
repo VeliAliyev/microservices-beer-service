8f7d74597a6fefd1ef052e1ee2e29f66cf8abbc1
VeliAliyev <veli_aliyev@outlook.com>
2022-12-15 16:18:44 +0300

    bootstrap class added

--- src/main/java/com/velialiyev/microservicesbeerservice/repositories/BeerRepository.java
+++ src/main/java/com/velialiyev/microservicesbeerservice/repositories/BeerRepository.java
@@ -1,9 +1,12 @@
 package com.velialiyev.microservicesbeerservice.repositories;
 
 import com.velialiyev.microservicesbeerservice.domain.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID>, JpaRepository<Beer, UUID> {
 }
