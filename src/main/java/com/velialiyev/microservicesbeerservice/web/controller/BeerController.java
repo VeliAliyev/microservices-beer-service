76858af9c019634c5bef728e0d5c8005017df9f0
VeliAliyev <veli_aliyev@outlook.com>
2022-12-19 11:13:35 +0300

    BeerController implemented

--- src/main/java/com/velialiyev/microservicesbeerservice/web/controller/BeerController.java
+++ src/main/java/com/velialiyev/microservicesbeerservice/web/controller/BeerController.java
@@ -1,6 +1,8 @@
 package com.velialiyev.microservicesbeerservice.web.controller;
 
+import com.velialiyev.microservicesbeerservice.services.BeerService;
 import com.velialiyev.microservicesbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -10,7 +12,10 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/beer")
+@RequiredArgsConstructor
 public class BeerController {
+
+    private final BeerService beerService;
 
 //    @GetMapping
 //    public ResponseEntity<BeerPagedList> listBeers(){
@@ -19,16 +24,21 @@ public class BeerController {
 
     @GetMapping("/{uuid}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID uuid){
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+        BeerDto found = beerService.getById(uuid);
+        return new ResponseEntity<>(found, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity createNewBeer(@RequestBody @Validated BeerDto beerDto){
-        return new ResponseEntity(HttpStatus.CREATED);
+
+        BeerDto saved = beerService.saveNewBeer(beerDto);
+
+        return new ResponseEntity(saved, HttpStatus.CREATED);
     }
 
     @PutMapping("/{uuid}")
-    public ResponseEntity<String> updateBeerById(@PathVariable UUID uuid, @RequestBody @Validated BeerDto beerDto){
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity updateBeerById(@PathVariable UUID uuid, @RequestBody @Validated BeerDto beerDto){
+        BeerDto updated = beerService.updateBeer(uuid, beerDto);
+        return new ResponseEntity<>(updated, HttpStatus.NO_CONTENT);
     }
 }
