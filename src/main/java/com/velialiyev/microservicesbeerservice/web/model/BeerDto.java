471e3ba9b8e835d1a06e0e6504a84e0021b3679d
VeliAliyev <veli_aliyev@outlook.com>
2022-12-19 10:50:38 +0300

    json config for BeerDto added

--- src/main/java/com/velialiyev/microservicesbeerservice/web/model/BeerDto.java
+++ src/main/java/com/velialiyev/microservicesbeerservice/web/model/BeerDto.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -31,6 +32,7 @@ public class BeerDto {
 
     @NotNull
     @Positive
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
 
     @Null
@@ -38,7 +40,9 @@ public class BeerDto {
     @Null
     private Integer version;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 }
